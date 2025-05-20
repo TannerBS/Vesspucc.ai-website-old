6d0bcc55ccefbf4e7411468bf9f8e58cee85ac42
sturgis <sturgis.steele@outlook.com>
2025-05-20 14:46:48 -0500

    fix repo base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Vespucc.ai-Website.git/",
+  base: "/Vespucci.ai-Website/", // Assuming your repository name is Vespucci.ai-Website
   plugins: [react()],
 })
