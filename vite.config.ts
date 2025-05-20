3ffb9270692fa6337f05af1823a5288bee28273b
sturgis <sturgis.steele@outlook.com>
2025-05-20 15:08:40 -0500

    correct name to vuspucc from vespucci

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Vespucci.ai-Website/", // Assuming your repository name is Vespucci.ai-Website
+  base: "/Vespucc.ai-Website/", // Assuming your repository name is Vespucci.ai-Website
   plugins: [react()],
 })
